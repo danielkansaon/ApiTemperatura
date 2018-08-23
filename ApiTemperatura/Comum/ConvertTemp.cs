01a2c8df2bc4f47db65b21d532fcb6347644dd9a
daniel.kansaon@mq.local <daniel.kansaon@mq.local>
2018-08-23 15:06:47 -0300

    Api-Temperatura

---
+++ ApiTemperatura/Comum/ConvertTemp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTemperatura.Comum
+{
+    public class ConvertTemp
+    {
+        public static double CelsiusToKelvin(double c)
+        {
+            return c + 273;
+        }
+
+        public static double KelvinToCelsius(double k)
+        {
+            return k - 273;
+        }
+
+        public static double CelsiusToFahrenheit(double c)
+        {
+            return c * 1.8 + 32;
+        }
+
+        public static double FahrenheitToCelsius(double f)
+        {
+            return (5.0 / 9.0) * (f - 32);
+        }
+    }
+}
---
+++ ApiTemperatura/Controllers/TemperaturaController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiTemperatura.Comum;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTemperatura.Controllers
+{
+    [Route("/api/v1/temperatura")]
+    public class TemperaturaController : Controller
+    {
+        [HttpGet(), Route("{grau}/from-celsius")]
+        public ActionResult GetFromCelsius(long c)
+        {
+            ConvertTemp.CelsiusToFahrenheit(c);
+            ConvertTemp.CelsiusToKelvin(c);
+
+            return View();
+        }
+
+        [HttpGet(), Route("{grau}/from-fahrenheit")]
+        public ActionResult GetFromFahrenheit(long f)
+        {
+            double celsius = ConvertTemp.FahrenheitToCelsius(f);
+            ConvertTemp.CelsiusToKelvin(celsius);
+
+            return View();
+        }
+
+        [HttpGet(), Route("{grau}/from-kelvin")]
+        public ActionResult GetFromKelvin(long k)
+        {
+            double kelvin = ConvertTemp.KelvinToCelsius(k);
+            ConvertTemp.CelsiusToFahrenheit(kelvin);
+            
+            return View();
+        }
+    }
+}
